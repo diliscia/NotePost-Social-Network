7fb892eda02fcd8f6738053da5d0891972d1eb90
Marcelo Di Liscia <diliscia@gmail.com>
2022-06-20 09:46:48 -0400

    update
    update

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const jwt = require("jsonwebtoken");
+// const database = require('./models');
 
 app.use(
   cors({
