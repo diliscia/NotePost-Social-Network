da7239a5b3fbf21cca0addf38d536a767f8a8ba1
Alidata <alidata127@gmail.com>
2022-06-22 17:37:21 -0400

    again available friends

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { useState, useEffect } from "react";
 import { UserContext } from "./components/user/UserContext";
-import AvailableFriends from "./components/friend/availableFriends";
+import AvailableFriends from "./components/friend/AvailableFriends";
 import MyProfile from "./components/user/myProfile";
 import EditProfile from "./components/user/editProfile";
 import EditImage from "./components/user/editImage";
@@ -31,7 +31,7 @@ function App() {
     <Router>
       <div className="containter">
         <Navbar bg="light" expand="lg">
-          <Container> 
+          <Container>
             <Navbar.Brand href="#home">PostNote</Navbar.Brand>
             <Navbar.Toggle />
             <Navbar.Collapse className="justify-content-end">
@@ -82,7 +82,7 @@ function App() {
             <Route exact path="/register" element={<Register />} />
             <Route exact path="/login" element={<Login />} />
             <Route element={<ProtectedRoutes />}>
-              <Route exact path="/" element={<Home />}/>
+              <Route exact path="/" element={<Home />} />
               <Route exact path="/add-article" element={<AddArticle />} />
               <Route exact path="/upload" element={<Upload />} />
               <Route exact path="/logout" element={<Logout />} />
