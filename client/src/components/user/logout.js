75086080dc89f522edf01a1fc70393f1e8a8e6c8
Marcelo Di Liscia <diliscia@gmail.com>
2022-06-24 09:27:20 -0400

    Update logout.js
    update

--- client/src/components/user/logout.js
+++ client/src/components/user/logout.js
@@ -6,7 +6,11 @@ function Logout() {
   const {user, setUser} = useContext(UserContext);
   localStorage.clear();
   setUser(null)
-
+return (
+  <div className="alert alert-success alert-dismissible fade show" role="alert" id="successAlert">
+  <strong>You have been logged out succesfully</strong>
+</div>
+);
 }
 
 export default Logout;
